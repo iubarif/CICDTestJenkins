28e77743f40538e7e12aad31a85d79b71020df16
Arif Mosharraf <arif.mosharraf@bcpharmacists.org>
2017-12-15 16:09:07 -0800

    sadd

--- CICDTest/UnitTestProject/UnitTest1.cs
+++ CICDTest/UnitTestProject/UnitTest1.cs
@@ -8,6 +8,19 @@ namespace UnitTestProject
         [TestMethod]
         public void TestMethod1()
         {
+            
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+
         }
     }
 }
